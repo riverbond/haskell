15b4f594db5b9e344d6e0b34e600de58057cfdb4
River Bond <riverbond.mail@gmail.com>
2017-04-15 15:23:52 -0600

    Added Haskell School of Music directory, minor changes in playground.

--- LYAH/playground.hs
+++ LYAH/playground.hs
@@ -4,17 +4,24 @@ doubleUs x y = doubleMe x + doubleMe y
 
 doubleSmallNumber x = if x > 100
                       then x
-                      else x*2
+                      else x * 2
 
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
 length' xs = sum [1 | _ <- xs]
 
-removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase :: String -> String
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
 
 addThree :: Int -> Int -> Int -> Int
 addThree x y z = x + y + z
 
-factorial :: Integer -> Integer
-factorial n = product [1..n]
+factorial :: (Integral a) => a -> a
+factorial 0 = 1
+factorial n = n * factorial (n - 1)
+
+circumference :: Float -> Float
+circumference r = 2 * pi * r
+
+circumference' :: Double -> Double
+circumference' r = 2 * pi * r
