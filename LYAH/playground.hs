498ee1a155e60ee67bf480c006135b753509273b
River Bond <riverbond.mail@gmail.com>
2017-04-16 12:45:33 -0600

    Cleaned up playground.hs

--- LYAH/playground.hs
+++ LYAH/playground.hs
@@ -1,14 +1,20 @@
+module Playground where
+
+doubleMe :: Num a => a -> a
 doubleMe x = x + x
 
+doubleUs :: Num a => a -> a -> a
 doubleUs x y = doubleMe x + doubleMe y
 
+doubleSmallNumber :: (Num a, Ord a) => a -> a
 doubleSmallNumber x = if x > 100
                       then x
                       else x * 2
 
+boomBangs :: Integral a => [a] -> [String]
 boomBangs xs = [ if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
 
-length' xs = sum [1 | _ <- xs]
+-- length' xs = sum [1 | _ <- xs]
 
 removeNonUppercase :: String -> String
 removeNonUppercase st = [ c | c <- st, c `elem` ['A'..'Z']]
@@ -18,10 +24,35 @@ addThree x y z = x + y + z
 
 factorial :: (Integral a) => a -> a
 factorial 0 = 1
-factorial n = n * factorial (n - 1)
+factorial n = n * factorial (n-1)
 
 circumference :: Float -> Float
 circumference r = 2 * pi * r
 
 circumference' :: Double -> Double
 circumference' r = 2 * pi * r
+
+head' :: [a] -> a
+head' []    = error "Empty List!"
+head' (x:_) = x
+
+length' :: (Num b) => [a] -> b
+length' []     = 0
+length' (_:xs) = 1 + length' xs
+
+max' :: (Ord a) => a -> a -> a
+max' a b
+    | a > b = a
+    | otherwise = b
+
+replicate' :: (Num i, Ord i) => i -> a -> [a]
+replicate' n x
+    | n <= 0    = []
+    | otherwise = x:replicate' (n-1) x
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) =
+    let smallerSorted = quicksort [a | a <- xs, a <= x]
+        biggerSorted  = quicksort [a | a <- xs, a > x]
+    in smallerSorted ++ [x] ++ biggerSorted
