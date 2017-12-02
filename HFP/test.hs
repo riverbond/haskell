74a70783663bc801299807ce1d1e4286903df73b
River Bond <riverbond.mail@gmail.com>
2017-12-02 12:09:04 -0700

    empty

--- HFP/test.hs
+++ HFP/test.hs
@@ -18,14 +18,15 @@ data Mood = Blah | Woot deriving Show
 
 changeMood :: Mood -> Mood
 changeMood Blah = Woot
-changeMood Woot = Blah
+changeMood    _ = Blah
 
 isPalindrome :: (Eq a) => [a] -> Bool
-isPalindrome x =
-  x == reverse x
+isPalindrome x = x == reverse x
 
 myAbs :: Integer -> Integer
 myAbs x = if x >= 0 then x else negate x
 
 f :: (a, b) -> (c, d) -> ((b, d), (a, c))
 f x y = ((snd x, snd y),  (fst x, fst y))
+
+-- TODO g (x:xs) = x pattern matching? type declaration?
