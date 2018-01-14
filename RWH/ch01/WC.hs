682124e23e3f2173cd35f583c23e3ee14d9fc86b
River Bond <riverbond.mail@gmail.com>
2018-01-14 16:04:04 -0700

    Minor changes

--- RWH/ch01/WC.hs
+++ RWH/ch01/WC.hs
@@ -3,6 +3,6 @@
 main :: IO ()
 main = interact wordCount
     where
-      wordCount input = show (length (lines input)) ++ "\n"
+        wordCount input = show (length (lines input)) ++ "\n"
 
 -- replace 'lines' with 'words' or remove it for # of characters
